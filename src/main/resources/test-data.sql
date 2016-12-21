3a0862062fac63854d06a246163a5b4f130493ba
Jeremy Buis <buis.jeremy@gmail.com>
2016-12-21 14:11:28 -0500

    added vuln code

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -5,3 +5,5 @@ insert into posts (title) values ('Ruby on Rails');
 insert into comments (text, post_id) values ('Java EE is too difficult and long to study', 1);
 insert into comments (text, post_id) values ('Ruby is very cool and easy to study', 2);
 insert into comments (text, post_id) values ('Ruby on Rails rocks and a must for Web development', 3);
+insert into comments (text, post_id) values ('<script>alert(1)</script>', 1);
+insert into comments (text, post_id) values ('background-url: url("http://google.com")', 1);
